c86b413329d13a66e01181b112ad5ccfdc671487
awalch6679 <alexander_walch@gmx.at>
2020-08-23 13:48:38 +0200

    upper bound for added noise shaping value

--- src/test1_main.cpp
+++ src/test1_main.cpp
@@ -3,14 +3,14 @@
 #include "Arduino.h"
 
 #define DEBUG
-//#define PLOTWAVE  //uncomment this line to plot the waveform (Arduino IDE Serial Plotter)
+#define PLOTWAVE  //uncomment this line to plot the waveform (Arduino IDE Serial Plotter)
 
 // Outputs
 AudioOutputSPDIF3        spdif3_1;       //xy=674,308
 Plotter                  plotter;
 
 // Inputs
-AsyncAudioInputSPDIF3    spdif_async1;//(false, false, 100, 20, 80); //dither = true, noiseshaping = true, anti-aliasing attenuation=100dB, minimum resampling filter length=20   //xy=408,225
+AsyncAudioInputSPDIF3    spdif_async1(true, true, 100, 20, 80); //dither = true, noiseshaping = true, anti-aliasing attenuation=100dB, minimum resampling filter length=20   //xy=408,225
 
 
 AudioSynthWaveform       waveform1;    
@@ -33,7 +33,7 @@ void setup()
   #endif
   AudioMemory(15); 
   waveform1.frequency(440);
-  waveform1.amplitude(0.9);
+  waveform1.amplitude(1);
 }
 
 void loop()
