78f409f15b2be1f9ec73a5dbc1dbc4d28db10df0
awalch6679 <alexander_walch@gmx.at>
2020-08-23 13:03:25 +0200

    first test without usb

--- src/test1_main_auxTeensy.cpp
+++ src/test1_main_auxTeensy.cpp
@@ -1,12 +1,52 @@
-// #include "Arduino.h"
+#include "plotter.h"
+#include "Audio.h" 
+#include "Arduino.h"
 
-// void setup()
-// {
-//   pinMode(LED_BUILTIN,OUTPUT);
-// }
+#define DEBUG
+#define PLOTWAVE  //uncomment this line to plot the waveform (Arduino IDE Serial Plotter)
 
-// void loop()
-// {
-//   digitalWriteFast(LED_BUILTIN,!digitalReadFast(LED_BUILTIN));
-//   delay(500);
-// }
+// Outputs
+AudioOutputSPDIF3        spdif3_1;       //xy=674,308
+Plotter                  plotter;
+
+// Inputs
+AsyncAudioInputSPDIF3    spdif_async1(false, false, 100, 20); //dither = true, noiseshaping = true, anti-aliasing attenuation=100dB, minimum resampling filter length=20   //xy=408,225
+
+
+AudioSynthWaveform       waveform1;    
+
+// waveform to SPDIF
+AudioConnection          patchCord1(waveform1, 0, spdif3_1, 0);
+AudioConnection          patchCord2(waveform1, 0, spdif3_1, 1);
+
+// waveform to plotter
+AudioConnection          patchCord3(spdif_async1, 0, plotter, 0);         
+
+void setup()
+{
+  #ifdef DEBUG
+    Serial.begin(115200);
+    while (!Serial);
+    #ifdef PLOTWAVE
+        plotter.activate(true);
+    #endif
+  #endif
+  AudioMemory(15); 
+  waveform1.frequency(440);
+  waveform1.amplitude(0.9);
+}
+
+void loop()
+{
+  #ifdef DEBUG
+  #ifndef PLOTWAVE
+    double bufferedTine=spdif_async1.getBufferedTime();
+    Serial.print("buffered time [micro seconds]: ");
+	Serial.println(bufferedTine*1e6,2);
+    double f=spdif_async1.getInputFrequency();
+	Serial.print("frequency: ");
+	Serial.println(f);
+  #endif
+  #endif
+  delay(500);
+}
